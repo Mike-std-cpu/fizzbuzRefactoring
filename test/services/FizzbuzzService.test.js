6e75c5a411ffd07e91e11b272733533c885df0a0
Mike&rade <hoodloom07@outlook.es>
2022-04-29 18:54:34 -0500

    10.1 Mejoras de código

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -14,7 +14,7 @@ describe("Pruebas de unidad de la clase Fizz Buzz Services:",() => {
                 validation[[false, false]] = false;
                 const score = 15;
 
-                fb_validation = validation[[score % 3 == 0, score % 5 == 0]];
+                const fb_validation = validation[[score % 3 == 0, score % 5 == 0]];
                 if(fb_validation){
                     fb_validation;
                 }else{
