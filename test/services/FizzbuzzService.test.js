f9ac56c340e55c5cae1603139ffae69c0e396690
Mike&rade <hoodloom07@outlook.es>
2022-04-30 02:17:25 -0500

    12. Creacion de pruebas FIZZ, BUZZ & FIZZBUZZ completas.

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -4,11 +4,30 @@ const FizzBuzzService = require("./../../lib/services/FizzbuzzService");
 
 describe("Pruebas de unidad de la clase Fizz Buzz Services:",() => {
    
-    test ("Prueba 1: Validación de FIZZBUZZ: ", () => {
+    test ("Prueba 1: Validación de SCORE = TRICK: ", () => {
         const explorer = {name: "Explorer1", score: 1};
-        FizzBuzzService.applyValidationExplorer(explorer); // {name: "Explorer1", score: 1, trick: 1}
+        FizzBuzzService.applyValidationExplorer(explorer);
         expect(explorer.trick).not.toBe(null);
         expect(explorer.trick).toBe(1);
+    });
 
+    test("Prueba 2: Validación de BUZZ | FIZZ :", () => {
+        const explorer3 = {name: "Explorer3", score: 3};
+        FizzBuzzService.applyValidationExplorer(explorer3);
+        expect(explorer3.trick).not.toBe(null);
+        expect(explorer3.trick).toBe("FIZZ");
+
+        const explorer5 = {name: "Explorer5", score: 5};
+        FizzBuzzService.applyValidationExplorer(explorer5);
+        expect(explorer5.trick).not.toBe(null);
+        expect(explorer5.trick).toBe("BUZZ");
+
+    });
+
+    test("Prueba 3: Validación de FIZZBUZZ :", () => {
+        const explorer15 = {name: "Explorer15", score: 15};
+        FizzBuzzService.applyValidationExplorer(explorer15);
+        expect(explorer15.trick).not.toBe(null);
+        expect(explorer15.trick).toBe("FIZZBUZZ");
     });
 });
