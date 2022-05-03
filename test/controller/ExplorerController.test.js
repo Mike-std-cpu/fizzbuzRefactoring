07268b5732733ba4b66b42b9163d05dfb962a51b
Mike&rade <hoodloom07@outlook.es>
2022-05-03 17:13:04 -0500

    14. Pruebas de metodos de filtrado de explorers.

--- test/controller/ExplorerController.test.js
+++ test/controller/ExplorerController.test.js
@@ -6,7 +6,19 @@ const { TestScheduler } = require("jest");
 describe("Pruebas de la clase EXPLORER CONTROLLER: ", () => {
     test("1.- Pruebas de la clase ExplorerConotroller metodo static: ", () => {
         const explorers = Reader.readJsonFile("explorers.json");
-    const myListExplorers = ExplorerServices.filterByMission(explorers,"node");
-    expect(myListExplorers).not.toBe(null);
+        const myListExplorers = ExplorerServices.filterByMission(explorers,"node");
+        expect(myListExplorers).not.toBe(null);
+    });
+
+    test("2.- Pruebas de metodos de la clase ExplorerServices: ", () => {
+        const explorers = Reader.readJsonFile("explorers.json");
+        const myListExplorers = ExplorerServices.filterByMission(explorers,"node");
+        const myNumberExplorers =ExplorerServices.getAmountOfExplorersByMission(explorers, "node");
+        const number = ExplorerServices.getExplorersUsernamesByMission(explorers,"node").length;
+        
+
+        expect(myListExplorers).not.toBe(null);
+        expect(myNumberExplorers).toBe(10);
+        expect(number).toBe(15);
     });
 });
